219ea75cda2d51d2904c5a8bf99d5d073010a3aa
conner12345678 <cehly065@west-mec.org>
2024-09-17 14:35:42 -0700

    this is not done

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ var app = express()
 const PORT = 5000
 
 const eventsData = require('./data/events.js')
+const peopleData = require('./data/people.js')
+
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   res.render('pages/index', {
-    events: eventsData
+    events: eventsData,
+    people: peopleData
+  })
+})
+app.get('/form', (req,res)=>{
+  res.render('pages/form', {
+    events: eventsData,
+    people: peopleData
   })
 })
 
