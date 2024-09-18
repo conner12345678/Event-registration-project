4de24695c200c4cca000d5d306d52350c33b8e12
conner12345678 <cehly065@west-mec.org>
2024-09-18 15:17:23 -0700

    update
    getting closer to the finish line

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = 5000
 const path = require('path')
 
-app.use(bodyParser.urlenclosed({ extended:true }))
+app.use(bodyParser.urlencoded({ extended:true }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
@@ -15,8 +15,13 @@ const getEvents = () => {
 }
 
 const saveEvents = (events) => {
-  fs.writeFileSync('./data/event.json', JSON.stringify(events, null, 2), 'utf8')
+  fs.writeFileSync('./data/events.json', JSON.stringify(events, null, 2), 'utf8')
 }
+
+app.get('/', (req,res) => {
+  const events = getEvents()
+  res.render('pages/index', { events })
+})
 
 
 app.get('/events', (req,res) => {
@@ -37,24 +42,24 @@ app.get('/events', (req,res) => {
 app.get('/events/:id/edit', (req,res) => {
   const events = getEvents()
   const event = events.find(event => event.id == req.params.id)
-  res.render('p', { p })
+  res.render('pages/form', { event })
 })
 
 app.post('/events/:id', (req,res) => {
   const events = getEvents()
-  const eventIndex = events.findIndex(task => task.id == req.params.id)
+  const eventIndex = events.findIndex(event => event.id == req.params.id)
   events[eventIndex].event = req.body.event
   events[eventIndex].dayOne = req.body.dayOne
-  events[eventIndex].dayTwo = req.body.dayTwo
   events[eventIndex].location = req.body.location
   events[eventIndex].address = req.body.address
+  events[eventIndex].names = req.body.names
   saveEvents(events)
   res.redirect('/')
 })
 
 app.post('/events/:id/delete', (req,res) => {
   let events = getEvents()
-  events = events.filter(task.id != req.params.id)
+  events = events.filter(event => event.id != req.params.id)
   saveEvents(events)
   res.redirect('/')
 })
